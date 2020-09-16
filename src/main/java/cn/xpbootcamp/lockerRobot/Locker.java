cc9680027ee9502fc37e0c1f96dc06cd0f24544f
Zi Ye <18612319597@163.com>
2020-09-16 20:52:38 +0800

    locker take the right bag

--- src/main/java/cn/xpbootcamp/lockerRobot/Locker.java
+++ src/main/java/cn/xpbootcamp/lockerRobot/Locker.java
@@ -1,17 +1,29 @@
 package cn.xpbootcamp.lockerRobot;
 
+import java.util.HashMap;
+
 public class Locker {
     private int capacity;
+    private HashMap<Receipt, Bag> bags = new HashMap<>();
 
     public Locker(int capacity) {
         this.capacity = capacity;
     }
 
     public Receipt saveBag(Bag bag) {
-        if(capacity != 0) {
-            capacity--;
-            return new Receipt();
+        if(hasAvailableCapacity()) {
+            Receipt receipt = new Receipt();
+            bags.put(receipt, bag);
+            return receipt;
         }
         throw new LockerIsFullException();
+    }
+
+    public Bag takeBag(Receipt receipt) {
+        return bags.get(receipt);
+    }
+
+    private boolean hasAvailableCapacity() {
+        return bags.size() < capacity;
     }
 }
--- src/test/java/cn/xpbootcamp/lockerRobot/LockerTest.java
+++ src/test/java/cn/xpbootcamp/lockerRobot/LockerTest.java
@@ -24,4 +24,15 @@ public class LockerTest {
             locker.saveBag(new Bag());
         });
     }
+
+    @Test
+    public void should_return_the_right_bag_when_take_bag_given_a_valid_receipt() {
+        Locker locker = new Locker(10);
+        Bag bag = new Bag();
+        Receipt receipt = locker.saveBag(bag);
+
+        Bag retrieveBag = locker.takeBag(receipt);
+
+        Assert.assertEquals(bag, retrieveBag);
+    }
 }
